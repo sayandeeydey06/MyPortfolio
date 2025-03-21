8fbec2d437f9559862d079642ae1d3ddcc88e1f3
sayandeepdey06 <sayandeepdey2919@gmail.com>
2025-03-21 23:15:46 +0530

    constant file removed from gitignore

---
+++ src/constants.js
@@ -0,0 +1,12 @@
+
+export const services = [
+];
+
+export const name = 'SAYANDEEP DEY';
+
+
+
+
+export const EMAIL_JS_SERVICE_ID = process.env.REACT_APP_EMAIL_JS_SERVICE_ID;
+export const EMAIL_JS_TEMPLATE_ID = process.env.REACT_APP_EMAIL_JS_TEMPLATE_ID;
+export const EMAIL_JS_PUBLIC_KEY = process.env.REACT_APP_EMAIL_JS_PUBLIC_KEY;

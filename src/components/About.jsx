0464cca0f8c2a28ce4b49638347ee084a82116e3
sayandeepdey06 <sayandeepdey2919@gmail.com>
2025-03-21 11:33:39 +0530

    MyPortfolio

---
+++ src/components/About.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import '../App.css';
+import { services } from '../constants';
+import ButtonLink from './ButtonLink';
+import Footer from './Footer';
+
+const ServiceCard = ({ service }) => (
+  <div className='sm:w-[250px] w-full'>
+    <div
+      className='w-full green-pink-gradient p-[1px] rounded-[20px]'>
+      <div
+        className='rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+        style={{ background: '#151030' }}>
+        <img
+          src={service.icon}
+          alt='some_icon'
+          className='w-16 h-16 object-contain'
+        />
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {service.title}
+        </h3>
+      </div>
+    </div>
+  </div>
+);
+
+const About = () => {
+  return (
+    <div>
+
+      <div className='bg-black h-full w-full text-white sm:flex sm:justify-around about py-12 mt-8 overflow-x-hidden' id='about'>
+        <div className='flex flex-col justify-around'>
+          <div className='sm:px-16 px-2'>
+            <h2 className='text-4xl sm:text-5xl font-extrabold mt-2'>Introduction</h2>
+            <p className='mt-3 mb-6 text-[17px] max-w-3xl leading-[30px]'>👨‍💻 Hi, I’m Sayandeep Dey a passionate frontend developer with a keen eye for design and a love for clean, efficient code. I specialize in building responsive, user-friendly web applications using modern technologies like React, JavaScript, HTML, and CSS. My goal is to create seamless digital experiences that are both functional and visually appealing.
+
+Let’s build something amazing together! 🚀
+
+
+
+
+
+
+
+ <a className='text-green-300 hover:text-green-500 duration-300' href='' target='_blank'>Frontend developer</a>
+              <br />
+              ✍️</p>
+
+            <ButtonLink
+          
+              text='View Resume →'
+              padding={`p-3`}
+            />
+
+          </div>
+          <div className='mt-20 flex justify-center flex-wrap gap-7'>
+            {services.map((service) => (
+              <ServiceCard service={service} />
+            ))}
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  )
+}
+
+export default About;
